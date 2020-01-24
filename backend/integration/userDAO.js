820d5ef1d4723b1699582399be0c7dad9bb225b8
jakmol@ug.kth.se <MmR-PZ-Q>
2020-01-24 15:42:11 +0100

    db deployment

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -2,13 +2,13 @@ const { Pool, Client } = require('pg')
 
 function connect() {
     const client = new Client({
-        // connectionString: process.env.DATABASE_URL,
-        user: "wlmremkduaitnk",
-        password: "83a43bfb610544a9c62da56a7144bafb13a726bf63a91e7ec454178a9623b479",
-        database: "d38bijitre5o3s",
-        port: 5432,
-        host: "ec2-54-247-92-167.eu-west-1.compute.amazonaws.com",
-        ssl: true
+        connectionString: process.env.DATABASE_URL,
+        // user: "wlmremkduaitnk",
+        // password: "83a43bfb610544a9c62da56a7144bafb13a726bf63a91e7ec454178a9623b479",
+        // database: "d38bijitre5o3s",
+        // port: 5432,
+        // host: "ec2-54-247-92-167.eu-west-1.compute.amazonaws.com",
+        // ssl: true
     });
     client.connect();
     return client
